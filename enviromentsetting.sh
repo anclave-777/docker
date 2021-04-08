d87f28b9af866d98652dfbda0ed9b84479c676da
anclave-777 <44027303+anclave-777@users.noreply.github.com>
2021-04-08 20:03:22 +0300

    Update enviromentsetting.sh

--- enviromentsetting.sh
+++ enviromentsetting.sh
@@ -7,6 +7,7 @@ sysctl -w net.ipv4.ip_forward=1
 
 # configure firewall
 iptables -t nat -A POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 iptables -A INPUT -i ppp+ -j ACCEPT
 iptables -A OUTPUT -o ppp+ -j ACCEPT
