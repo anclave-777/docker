54d899114e6da2311888b6bf42ab325a346d97a7
anclave-777 <44027303+anclave-777@users.noreply.github.com>
2025-12-21 11:58:50 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,36 +7,56 @@ KEYS=$(xray x25519)
 PRIVATE_KEY=$(echo "$KEYS" | grep "Private key:" | awk '{print $3}')
 PUBLIC_KEY=$(echo "$KEYS" | grep "Public key:" | awk '{print $3}')
 
-# Создаем конфиг
-cat > /etc/xray/config.json <<EOL
 {
-    "log": {"loglevel": "warning"},
-    "inbounds": [{
-        "port": 443,
-        "protocol": "vless",
-        "settings": {
-            "clients": [{
-                "id": "$UUID",
-                "flow": "xtls-rprx-vision"
-            }],
-            "decryption": "none"
-        },
-        "streamSettings": {
-            "network": "http",
-            "security": "reality",
-            "realitySettings": {
-                "serverName": "google.com" ,
-                "fingerprint": "firefox",
-                "privateKey": "$PRIVATE_KEY",
-                "shortIds": ["0a381e1f", "be0ce047"],          
-                "spiderX": "/"
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": 4443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",          // здесь вставить uuid, сгенерированный на шаге №2
+                        "flow": "xtls-rprx-vision"
+                    }
+                ],
+                "decryption": "none"
             },
-        "httpSettings": {
-          "host": [""],
-          "path": "/"
+            "streamSettings": {
+                "network": "tcp",
+                "security": "reality",
+                "realitySettings": {
+                    "dest": "ya.ru:443",        // здесь заменить `example.com` на ваш сайт из шага №3, порт оставляем
+                    "serverNames": [
+                        "ya.ru",                // заменить на ваш сайт из шага №3
+                        "www.ya.ru"   // и добавить альтернативные имена (если есть)
+                    ],
+                    "privateKey": "$PRIVATE_KEY",// вставить приватный ключ (Private key), созданный на шаге №2
+                    "shortIds": [
+                        "0a381e1fa219",         // Список уникальных коротких идентификаторов, доступных клиентам, чтобы их различать
+                        "be0ce04754dc",         // Длина: от 2 до 16 символов. Используемые символы: 0-f.
+                        "41beec74f4bc"          // Для удобства, значения можно сгенерировать командой `openssl rand -hex 6`
+                    ]
+                }
+            },
+            "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ]
+            }
         }
-    }],
-    "outbounds": [{"protocol": "freedom"}]
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        }
+    ]
+}
 }
 EOL
 
